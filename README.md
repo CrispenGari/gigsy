7ecbf9ca8df178ee90fb061a5c4db45f3edb3944
Crispen Gari <crispengari@gmail.com>
2023-12-15 10:11:48 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# LocalWorkMate
+### Local Work Mate
+
 👷‍♀️ This is a simple mobile application that allows users to advertise piece works/jobs in their location/city.
---
+++ src/constants/index.ts
@@ -0,0 +1,30 @@
+export const APP_NAME = "Local Work Mate";
+
+export const KEYS = {
+  APP_SETTINGS: "set:",
+  USER: "user:",
+};
+export const COLORS = {
+  main: "#163020",
+  primary: "#304D30",
+  secondary: "#B6C4B6",
+  tertiary: "#EEF0E5",
+  white: "white",
+  red: "#FF3953",
+  black: "#000000",
+  gray: "#f5f5f5",
+};
+export const Fonts = {
+  MontserratAlternatesItalic: require("../../assets/fonts/MontserratAlternates-Italic.ttf"),
+  MontserratAlternatesRegular: require("../../assets/fonts/MontserratAlternates-Regular.ttf"),
+  MontserratAlternatesBold: require("../../assets/fonts/MontserratAlternates-Bold.ttf"),
+  MontserratAlternatesBoldItalic: require("../../assets/fonts/MontserratAlternates-BoldItalic.ttf"),
+};
+export const FONTS = {
+  regular: "MontserratAlternatesRegular",
+  italic: "MontserratAlternatesItalic",
+  italicBold: "MontserratAlternatesBoldItalic",
+  regularBold: "MontserratAlternatesBold",
+};
+
+export const logo = require("../../assets/logo.png");
